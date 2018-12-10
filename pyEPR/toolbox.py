be6822f4f804534d7905552b2258c6a242bb2b89
hpaik1 <hanhee.paik@gmail.com>
2018-12-10 18:49:24 -0500

    toolbox: added rounding

--- pyEPR/toolbox.py
+++ pyEPR/toolbox.py
@@ -50,6 +50,9 @@ def isfloat(value):
   except ValueError:
     return False
 
+def floor_10(x):
+    ''' round to nearest lower power of 10 c'''
+    return  10.**(np.floor(np.log10(x)))
 
 def sort_df_col(df):
     '''         sort by numerical int order    '''
@@ -73,7 +76,6 @@ def get_instance_vars(obj, Forbidden=[]):
     VARS = {}
     for v in dir(obj):
         if not ((v.startswith('__')) or (v.startswith('_'))):
-            print(v)
             if not callable(getattr(obj,v)):
                 if not (v in Forbidden):
                     VARS[v] = getattr(obj,v)
