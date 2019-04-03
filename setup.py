e8ba112a7e754e797654e828dd3168859d4faca7
Zlatko Minev <zminev@gmail.com>
2019-04-02 22:53:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-"""Experiment control and data acquisition software for the Parity measurment.
+"""
+      Python (py) Energy-Participation-Ratio (EPR) package
 """
 
 from setuptools import setup, find_packages
@@ -6,12 +7,12 @@ from setuptools import setup, find_packages
 doclines = __doc__.split('\n')
 
 setup(name='pyEPR',
-      version='0.0',
+      version='0.6',
       description = doclines[0],
       long_description = '\n'.join(doclines[2:]),
       author='Zlatko Minev',
       packages=['pyEPR'],
-      author_email='???',
-      license='???',
+      author_email='zminev@gmail.com',
+      license=' BSD-3-Clause',
       install_requires=['numpy','pandas','pint','matplotlib']
       )
