164d077e6e76fd326f046d2730e8a11fd3904cc3
Zlatko K. Minev <zminev@gmail.com>
2019-07-16 23:11:57 -0400

    logger update

--- pyEPR/__init__.py
+++ pyEPR/__init__.py
@@ -31,37 +31,44 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
-### Compatibility with python 2.7 and 3
+# Compatibility with python 2.7 and 3
 from __future__ import division, print_function, absolute_import
+from collections import OrderedDict
 
+import logging
 __imports_warn = False
 
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-### Configure logging 
-import logging
-logger = logging.getLogger('pyEPR') # singleton
+##############################################################################
+### Configure logging
+
+logger = logging.getLogger('pyEPR')  # singleton
+
 if not len(logger.handlers):
     c_handler = logging.StreamHandler()
     logger.propagate = False
     # Jupyter notebooks already has a stream handler on the default log,
     # Do not propage upstream to the root logger.  https://stackoverflow.com/questions/31403679/python-logging-module-duplicated-console-output-ipython-notebook-qtconsole
-        
-    # Format 
-    c_handler.setLevel(logging.WARNING)
-    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s\n   ::%(pathname)s:%(lineno)d: %(funcName)s\n')  #  unlike the root logger, a custom logger can’t be configured using basicConfig(). 
+
+    # Format
+    # unlike the root logger, a custom logger can’t be configured using basicConfig().
+    #c_format = logging.Formatter(
+    #    '%(name)s - %(levelname)s - %(message)s\n   ::%(pathname)s:%(lineno)d: %(funcName)s\n')
+    c_format = logging.Formatter('%(asctime)s [%(funcName)s]: %(message)s',
+                                 datefmt='%I:%M%p %Ss')
+
     c_handler.setFormatter(c_format)
     logger.addHandler(c_handler)
-    
-    
+    logger.setLevel(logging.INFO)
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+##############################################################################
 # Import Checks Matplotlib & core packages
 __STD_END_MSG = """\n   If you need a part of pyEPR that uses this package,
    then please install it. Then add it to the system path (if needed). 
    See online setup instructions at 
    github.com/zlatko-minev/pyEPR"""
-   
+
 try:
     import matplotlib as mpl
 except (ImportError, ModuleNotFoundError):
@@ -69,13 +76,14 @@ except (ImportError, ModuleNotFoundError):
    Could not find package `matplotlib`. 
    Default plotting will not work unless you install it. """ + __STD_END_MSG)
     raise(ImportError("Please install python package `matplotlib`"))
-    
-    
+
+
 try:
     import pandas as pd
     import warnings
-    warnings.filterwarnings('ignore', category=pd.io.pytables.PerformanceWarning)
-except (ImportError, ModuleNotFoundError):    
+    warnings.filterwarnings(
+        'ignore', category=pd.io.pytables.PerformanceWarning)
+except (ImportError, ModuleNotFoundError):
     if __imports_warn:
         logger.warning("""IMPORT WARNING:
    `pandas` python package not found.""" + __STD_END_MSG)
@@ -91,9 +99,9 @@ except (ImportError, ModuleNotFoundError):
  
    You could try `conda install -c conda-forge qutip`
                    """ + __STD_END_MSG)
-#else:
+# else:
 #    del qutip, warnings
-            
+
 # A few usually troublesome packages
 try:
     import pythoncom
@@ -105,7 +113,7 @@ except (ImportError, ModuleNotFoundError):
    For Linux, check the HFSS python linux files for the com module used. It is equivalent, 
    and can be used just as well. 
    """ + __STD_END_MSG)
-    
+
 try:
     from win32com.client import Dispatch, CDispatch
 except (ImportError, ModuleNotFoundError):
@@ -114,9 +122,9 @@ except (ImportError, ModuleNotFoundError):
    Could not load from 'win32com.client'.
    The communication to hfss won't work. If you want to use it, you need to set it up. 
    """ + __STD_END_MSG)
-    
+
 try:
-    from pint import UnitRegistry # units
+    from pint import UnitRegistry  # units
 except (ImportError, ModuleNotFoundError):
     if __imports_warn:
         logger.error("""IMPORT ERROR: 
@@ -127,11 +135,10 @@ except (ImportError, ModuleNotFoundError):
         #raise(ImportError("Please install python package `pint`"))
 
 
+##############################################################################
+# pyEPR Specific
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# pyEPR Specific 
-    
-### Config setup
+# Config setup
 from . import config
 try:  # Check if we're in IPython.
     __IPYTHON__
@@ -139,21 +146,16 @@ try:  # Check if we're in IPython.
 except:
     config.ipython = False
 config.__STD_END_MSG = __STD_END_MSG
-    
 
-### Convenience variable and function imports
-from . import hfss
-from . import core
-from . import numeric_diag
-from . import toolbox
-from . import toolbox_circuits
-from . import toolbox_plotting
 
-from .hfss import load_HFSS_project, get_active_design, get_active_project,\
-                  HfssProject
-from .hfss import release as hfss_release
+# Convenience variable and function imports
 from .core import Project_Info, pyEPR_HFSS, pyEPR_Analysis
-
-from collections import OrderedDict
-#from .toolbox import *
-#from .toolbox_plotting import *
+from .hfss import release as hfss_release
+from .hfss import load_HFSS_project, get_active_design, get_active_project,\
+    HfssProject
+from . import toolbox_plotting
+from . import toolbox_circuits
+from . import toolbox
+from . import numeric_diag
+from . import core
+from . import hfss
