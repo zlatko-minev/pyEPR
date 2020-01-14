3222570259d4531e45806c15d8cf8db6cc0d35eb
Zlatko K. Minev <zlatko.minev@aya.yale.edu>
2020-01-14 17:56:03 -0500

    Cleanup code for hamiltonian calcs - no significant code changes

--- pyEPR/calcs/hamiltonian.py
+++ pyEPR/calcs/hamiltonian.py
@@ -5,23 +5,25 @@ This package must be installded for them to work.
 """
 try:
     import qutip
-    from qutip import QObj #  basis, tensor,
+    from qutip import Qobj  # basis, tensor,
 except (ImportError, ModuleNotFoundError):
+    Qobj=None
     pass
 
 from ..toolbox.pythonic import fact
 
+
 class MatrixOps(object):
 
     @staticmethod
-    def cos(op_cos_arg:QObj):
+    def cos(op_cos_arg: Qobj):
         """
         Make cosine opertor matrix from arguemnt  op_cos_arg
 
-            op_cos_arg (qutip.QObj) : argumetn of the cosine
+            op_cos_arg (qutip.Qobj) : argumetn of the cosine
         """
 
-        return 0.5*( (1j*op_cos_arg).expm() + (-1j*op_cos_arg).expm() )
+        return 0.5*((1j*op_cos_arg).expm() + (-1j*op_cos_arg).expm())
 
     @staticmethod
     def cos_approx(x, cos_trunc=5):
@@ -38,37 +40,52 @@ class MatrixOps(object):
         return sum(ai*bi for ai, bi in zip(ais, bis))
 
 
-
 class HamOps(object):
+
     @staticmethod
-    def fock_state_on(d, fock_trunc, N_modes):
+    def fock_state_on(d: dict, fock_trunc: int, N_modes: int):
         ''' d={mode number: # of photons} In the bare eigen basis
         '''
-        return qutip.tensor(*[qutip.basis(fock_trunc, d.get(i, 0)) for i in range(N_modes)])  # give me the value d[i]  or 0 if d[i] does not exist
+        # give me the value d[i]  or 0 if d[i] does not exist
+        return qutip.tensor(*[qutip.basis(fock_trunc, d.get(i, 0))
+                              for i in range(N_modes)])
 
     @staticmethod
-    def closest_state_to(s, energyMHz, evecs):
+    def closest_state_to(s: Qobj, energyMHz, evecs):
+        """
+        Returns the enery of the closest state to s
+        """
         def distance(s2):
             return (s.dag() * s2[1]).norm()
         return max(zip(energyMHz, evecs), key=distance)
 
     @staticmethod
-    def closest_state_to_idx(s, evecs):
+    def closest_state_to_idx(s: Qobj, evecs):
+        """
+        Returns the index
+        """
         def distance(s2):
             return (s.dag() * s2[1]).norm()
         return max(zip(range(len(evecs)), evecs), key=distance)
 
     @staticmethod
-    def identify_Fock_levels(fock_trunc, #= my_params['Nmax']
-                         evecs,
-                         N_modes = 2,
-                         Fock_max = 4):
-        fock_state_on        = lambda d: HamOps.fock_state_on(d, fock_trunc, N_modes)
-        closest_state_to_idx = lambda s: HamOps.closest_state_to_idx(s, evecs)
+    def identify_Fock_levels(fock_trunc: int, evecs,
+                             N_modes=2,
+                             Fock_max=4):
+        """
+        Return quantum numbers in terms of the undiagonalized eigenbasis.
+        """
+        #  to do: need to turn Fock_max into arb algo on each mdoe
 
-        FOCKr = {} #TODO:# need to turn into arb algo
+        def fock_state_on(d):
+            return HamOps.fock_state_on(d, fock_trunc, N_modes)
+
+        def closest_state_to_idx(s):
+            return HamOps.closest_state_to_idx(s, evecs)
+
+        FOCKr = {}
         for d1 in range(Fock_max):
             for d2 in range(Fock_max):
-                d = {0:d1, 1:d2}
+                d = {0: d1, 1: d2}
                 FOCKr[closest_state_to_idx(fock_state_on(d))[0]] = d
         return FOCKr
