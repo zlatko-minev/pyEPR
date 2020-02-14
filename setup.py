da607157ebd242028483930980696291c701c740
Zlatko Minev <zminev@gmail.com>
2020-02-13 20:52:11 -0500

    docs

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='pyEPR',
       packages=find_packages(),
       author_email='zlatko.minev@aya.yale.edu',
       license='BSD-3-Clause',
-      install_requires=['numpy','pandas','pint','matplotlib','attrdict','sympy'],
+      install_requires=['numpy','pandas','pint','matplotlib','attrdict','sympy','IPython'],
       url=r'https://github.com/zlatko-minev/pyEPR'
       )
