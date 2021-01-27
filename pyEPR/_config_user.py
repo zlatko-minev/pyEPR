fe8c50b212388f5b137b0a828dc0472d36cc80a9
Dennis Wang <Dennis.Wang@ibm.com>
2021-01-27 15:08:20 -0500

    Update config_user.py

--- pyEPR/_config_user.py
+++ pyEPR/_config_user.py
@@ -21,7 +21,7 @@ config = Dict( # pylint: disable=invalid-name
 
     # Folder to save result data to.
     # PLEASE CHANGE THIS
-    root_dir=r'D:\data-pyEPR',
+    root_dir=r'C:\data-pyEPR', # Not all machines have a D drive so substituting D with C here
 
     # Loss properties of various materials and surfaces
     dissipation=Dict(
