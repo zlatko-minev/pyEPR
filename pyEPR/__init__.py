59456283d8927855aae3024c53ae61f3990aa306
Priti A Shah <Priti.Ashvin.Shah@ibm.com>
2022-02-10 14:10:49 -0500

    Use updated tag to include fix for issue #96.

--- pyEPR/__init__.py
+++ pyEPR/__init__.py
@@ -59,7 +59,7 @@ Automated analysis of lumped and distributed circuits is provided.
 @author: Zlatko Minev, Zaki Leghas, ... and the pyEPR team
 @site: https://github.com/zlatko-minev/pyEPR
 @license: "BSD-3-Clause"
-@version: 0.8.5.2
+@version: 0.8.5.3
 @maintainer: Zlatko K. Minev and  Asaf Diringer
 @email: zlatko.minev@aya.yale.edu
 @url: https://github.com/zlatko-minev/pyEPR
@@ -92,7 +92,7 @@ __credits__ = [
     "Will Livingston", "Steven Touzard"
 ]
 __license__ = "BSD-3-Clause"
-__version__ = "0.8.5.2"
+__version__ = "0.8.5.3"
 __maintainer__ = "Zlatko K. Minev and  Asaf Diringer"
 __email__ = "zlatko.minev@aya.yale.edu"
 __url__ = r'https://github.com/zlatko-minev/pyEPR'
