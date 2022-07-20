f05295d475aa53cae1711ea14521b1ff0810fac4
Zach Parrott <51793790+zachparrott@users.noreply.github.com>
2022-07-20 09:34:17 -0600

    Add other parametric sweep options (#117)
    * fix keyboard mash?
    
    * Implemented all available options for parametric sweep definitions and parametric sweep from file.
    
    * Addition of demo tutorial and associated csv file

--- pyEPR/ansys.py
+++ pyEPR/ansys.py
@@ -592,7 +592,7 @@ class HfssProject(COMWrapper):
         """Create a new driven model design
 
         Args:
-            name (str): Name of driven modal design    
+            name (str): Name of driven modal design
         """
         return self.new_design(name, "DrivenModal")
 
@@ -666,7 +666,7 @@ class HfssDesign(COMWrapper):
         self._modeler.ExportModelImageToFile(
             str(path),
             0,
-            0,  # can be 0 For the default, use 0, 0. For higher resolution, set desired <width> and <height>, for example for 8k export as: 7680, 4320. 
+            0,  # can be 0 For the default, use 0, 0. For higher resolution, set desired <width> and <height>, for example for 8k export as: 7680, 4320.
             [
                 "NAME:SaveImageParams", "ShowAxis:=", "True", "ShowGrid:=",
                 "True", "ShowRuler:=", "True", "ShowRegion:=", "Default",
@@ -1881,50 +1881,140 @@ class Optimetrics(COMWrapper):
                      solve_with_copied_mesh_only=True,
                      setup_type='parametric'):
         """
-        Inserts a new parametric setup.
-
-
-        For  type_='linear_step' swp_params is start, stop, step:
-             swp_params = ("12.8nH" "13.6nH", "0.2nH")
+        Inserts a new parametric setup of one variable. Either with sweep 
+        definition or from file.
 
         Corresponds to ui access:
-            Right-click the Optimetrics folder in the project tree,
-            and then click Add> Parametric on the shortcut menu.
+        Right-click the Optimetrics folder in the project tree, and then click 
+        Add> Parametric on the shortcut menu.
+
+        Ansys provides six sweep definitions types specified using the swp_type
+        variable.
+
+        Sweep type definitions:
+        - 'single_value'          	
+            Specify a single value for the sweep definition.
+        - 'linear_step'
+            Specify a linear range of values with a constant step size.
+        - 'linear_count'
+            Specify a linear range of values and the number, or count of points
+            within this range.
+        - 'decade_count'
+            Specify a logarithmic (base 10) series of values, and the number of
+            values to calculate in each decade.
+        - 'octave_count'
+            Specify a logarithmic (base 2) series of values, and the number of 
+            values to calculate in each octave.
+        - 'exponential_count'
+            Specify an exponential (base e) series of values, and the number of 
+            values to calculate.
+
+        For swp_type='single_value' swp_params is the single value.
+
+        For  swp_type='linear_step' swp_params is start, stop, step:
+            swp_params = ("12.8nH", "13.6nH", "0.2nH")
+        
+        All other types swp_params is start, stop, count:
+            swp_params = ("12.8nH", "13.6nH", 4)
+            The definition of count varies amongst the available types. 
+
+        For Decade count and Octave count, the Count value specifies the number
+        of points to calculate in every decade or octave. For Exponential count,
+        the Count value is the total number of points. The total number of 
+        points includes the start and stop values.
+
+        For parametric from file, setup_type='parametric_file', pass in a file
+        name and path to swp_params like "C:\\test.csv" or "C:\\test.txt" for 
+        example.
+
+        Example csv formatting:
+        *,Lj_qubit
+        1,12.2nH
+        2,9.7nH
+        3,10.2nH
+
+        See Ansys documentation for additional formatting instructions. 
         """
         setup_name = setup_name or self.design.get_setup_names()[0]
         print(
             f"Inserting optimetrics setup `{name}` for simulation setup: `{setup_name}`"
         )
 
-        if setup_type != 'parametric':
-            raise NotImplementedError()
+        if setup_type == 'parametric':
+            valid_swp_types = ['single_value', 'linear_step', 'linear_count', 
+            'decade_count', 'octave_count', 'exponential_count']
 
-        if swp_type == 'linear_step':
-            assert len(swp_params) == 3
-            # e.g., "LIN 12.8nH 13.6nH 0.2nH"
-            swp_str = f"LIN {swp_params[0]} {swp_params[1]} {swp_params[2]}"
+            if swp_type not in valid_swp_types:
+                raise NotImplementedError()
+            else:
+                if swp_type == 'single_value':
+                    # Single takes string of single variable no swp_type_name
+                    swp_str = f"{swp_params}"
+
+                else:
+                    # correct number of inputs
+                    assert len(swp_params) == 3, "Incorrect number of sweep parameters."
+
+                    # Not checking for compatible unit types
+                    if swp_type == 'linear_step':
+                        swp_type_name = "LIN"
+                    else:
+                        # counts needs to be an integer number
+                        assert isinstance(swp_params[2], int), "Count must be integer."
+
+                        if swp_type == 'linear_count':
+                            swp_type_name = "LINC"
+                        elif swp_type == 'decade_count':
+                            swp_type_name = "DEC"
+                        elif swp_type == 'octave_count':
+                            swp_type_name = "OCT"
+                        elif swp_type == 'exponential_count':
+                            swp_type_name = "ESTP"
+
+                    # prepare the string to pass to Ansys
+                    swp_str = f"{swp_type_name} {swp_params[0]} {swp_params[1]} {swp_params[2]}"
+
+            # talk with Ansys
+            self._optimetrics.InsertSetup("OptiParametric", [
+                f"NAME:{name}", "IsEnabled:=", True,
+                [
+                    "NAME:ProdOptiSetupDataV2",
+                    "SaveFields:=",
+                    save_fields,
+                    "CopyMesh:=",
+                    copy_mesh,
+                    "SolveWithCopiedMeshOnly:=",
+                    solve_with_copied_mesh_only,
+                ], ["NAME:StartingPoint"], "Sim. Setups:=", [setup_name],
+                [
+                    "NAME:Sweeps",
+                    [
+                        "NAME:SweepDefinition", "Variable:=", variable, "Data:=",
+                        swp_str, "OffsetF1:=", False, "Synchronize:=", 0
+                    ]
+                ], ["NAME:Sweep Operations"], ["NAME:Goals"]
+            ])
+        elif setup_type == 'parametric_file':
+            # Uses the file name as the swp_params 
+            filename = swp_params
+
+            self._optimetrics.ImportSetup("OptiParametric",
+                [
+                f"NAME:{name}",
+                filename,
+                ])
+            self._optimetrics.EditSetup(f"{name}",
+                [
+                    f"NAME:{name}",
+            		[
+            			"NAME:ProdOptiSetupDataV2",
+            			"SaveFields:="		, save_fields,
+            			"CopyMesh:="		, copy_mesh,
+            			"SolveWithCopiedMeshOnly:=", solve_with_copied_mesh_only,
+            		],
+            ])
         else:
             raise NotImplementedError()
-
-        self._optimetrics.InsertSetup("OptiParametric", [
-            f"NAME:{name}", "IsEnabled:=", True,
-            [
-                "NAME:ProdOptiSetupDataV2",
-                "SaveFields:=",
-                save_fields,
-                "CopyMesh:=",
-                copy_mesh,
-                "SolveWithCopiedMeshOnly:=",
-                solve_with_copied_mesh_only,
-            ], ["NAME:StartingPoint"], "Sim. Setups:=", [setup_name],
-            [
-                "NAME:Sweeps",
-                [
-                    "NAME:SweepDefinition", "Variable:=", variable, "Data:=",
-                    swp_str, "OffsetF1:=", False, "Synchronize:=", 0
-                ]
-            ], ["NAME:Sweep Operations"], ["NAME:Goals"]
-        ])
 
 
 class HfssModeler(COMWrapper):
